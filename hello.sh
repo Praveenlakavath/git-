682960f0a1f17146b8531e40f13d9cc720acd5a0
praveen <praveenlakavath@7989>
2022-09-27 13:47:23 +0000

    done

---
+++ hello.sh
@@ -0,0 +1,3 @@
+echo 'msg1="hellow worldd" ' 
+echo 'echo $msg1 >> hello
+chmod 700 hello
---
+++ pro1.sh
@@ -0,0 +1,14 @@
+clear
+echo 'please enter your name'
+read   'name'
+echo 'hi ! welcome to my world'
+echo 'Todays date'
+echo ''
+date
+msg1=hellow
+msg2=there
+echo $msg1 $msg2
+msg1="one"
+msg2="$msg1 two"
+msg3="$msg2 three"
+echo $msg3
